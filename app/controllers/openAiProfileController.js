35d3607d2fd81e9cc71c70721fa937597e5b1e50
Zach <shaw.zacht@gmail.com>
2023-10-09 23:17:07 -0700

    initial functionality for AI generated profile data

---
+++ app/controllers/openAiProfileController.js
@@ -0,0 +1,41 @@
+
+const { OpenAI } = require('openai');
+
+const openai = new OpenAI({
+  apiKey: "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"
+});
+
+async function generateProfile(req, res) {
+
+    const prompt = `Dating app picture, photo realistic, hyper realistic, 34 year old, brunette, attractive, alluring, woman, sigma 24 mm f/8 lens, smiling, loves the beach`
+
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+          messages: [{ role: 'user', content: `create a dating profile with name, age and 25 word bio using this prompt: ${prompt}` }],
+          model: 'gpt-3.5-turbo',
+        });
+
+        const response = chatCompletion.choices
+
+        res.status(200).json({
+            success: true,
+            data: response
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: 'The profile cannot be generated'
+        })
+
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message)
+        }
+        
+    }
+  }
+
+module.exports = { generateProfile }
--- app/routes/openAiRoutes.js
+++ app/routes/openAiRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { generateImage } = require('../controllers/openAiController')
+const { generateProfile } = require('../controllers/openAiProfileController')
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
+router.post('/generateprofile', generateProfile);
 
 module.exports = router;
