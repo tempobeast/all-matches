ddde8d765e910416224ccbba17e790affe8053ce
Zach <shaw.zacht@gmail.com>
2023-10-23 20:59:49 -0700

    on logo click return to questionaire page;

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,15 @@
 import "../App.css";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
 
+  const navigate = useNavigate();
+
   return (
     <div className="header">
-        <img className="header__flame" src="/all_matches_logo.png"/>
+        <img className="header__flame" src="/all_matches_logo.png" onClick={() => navigate("/")}/>
     </div>
   );
 }
