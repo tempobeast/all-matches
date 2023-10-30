90ae5233bf6b2a1aef321ad04bf8ba12e96db8b3
Zach <shaw.zacht@gmail.com>
2023-10-30 15:55:41 -0700

    init helpers, abstract seed data

---
+++ client/src/helpers/promptDataSeeds.js
@@ -0,0 +1,27 @@
+  //seed data to randomize match
+    export const hairColor = [
+        "redhead",
+        "blonde",
+        "brunette",
+        "blonde",
+        "brunette",
+        "brunette",
+        "blonde",
+      ];
+     export const location = [
+        "at the beach",
+        "at a sporting event",
+        "in a forest",
+        "on a sailboat",
+        "at a party",
+        "on a train",
+        "backpacking",
+        "visiting a world monument"
+      ];
+      
+      export const race = ["black", "caucasian", "latinx", "asian"];
+    
+    
+
+
+
